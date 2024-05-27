1f4f361aaa198748c42bc3e59a37fc127b3c3a07
Dauren <213053@astanait.edu.kz>
2024-05-27 20:13:09 +0500

    updated base url for frontend

--- client/src/shared/api/http.ts
+++ client/src/shared/api/http.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const http = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/v1/',
+  baseURL: 'https://daufood.vercel.app/api/v1/',
   headers: {
     'Content-Type': 'application/json'
   }
